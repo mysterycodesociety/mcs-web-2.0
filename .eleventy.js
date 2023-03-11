91eb945dbf2ce3560d69e05f9b22d9a21243f151
Morgan Haworth <morgan123haworth@gmail.com>
2023-03-11 11:55:25 -0600

    Upgrade Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,11 @@ const { DateTime } = require("luxon");
 const slugify = require("slugify");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+    strictFilters: false,
+  });
 
   // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -12,28 +16,27 @@ module.exports = function(eleventyConfig) {
   // layout: post. If you don’t want to rewrite all of those values, just map
   // post to a new file like this:
   // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
-  eleventyConfig.addLayoutAlias('default', `layouts/default.njk`);
+  eleventyConfig.addLayoutAlias("default", `layouts/default.njk`);
 
   // Merge data instead of overriding
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
 
   // Date formatting (human readable)
-  eleventyConfig.addFilter("readableDate", dateObj => {
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
   });
 
   // Date formatting (machine readable)
-  eleventyConfig.addFilter("machineDate", dateObj => {
+  eleventyConfig.addFilter("machineDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
   });
 
-
-  eleventyConfig.addCollection("posts", function(collection){
+  eleventyConfig.addCollection("posts", function (collection) {
     return collection.getFilteredByGlob(["posts/*"]);
   });
 
-  eleventyConfig.addCollection("pages", function(collection){
+  eleventyConfig.addCollection("pages", function (collection) {
     return collection.getFilteredByGlob(["pages/*md", "pages/*liquid"]);
   });
 
@@ -50,23 +53,23 @@ module.exports = function(eleventyConfig) {
   });
 
   // Universal slug filter strips unsafe chars from URLs
-  eleventyConfig.addFilter("slugify", function(str) {
+  eleventyConfig.addFilter("slugify", function (str) {
     return slugify(str, {
       lower: true,
       replacement: "-",
-      remove: /[*+~.·,()'"`´%!?¿:@]/g
+      remove: /[*+~.·,()'"`´%!?¿:@]/g,
     });
   });
 
   // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy({'assets': 'assets' });
+  eleventyConfig.addPassthroughCopy({ assets: "assets" });
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/blog");
   eleventyConfig.addPassthroughCopy("_includes/assets/images");
   eleventyConfig.addPassthroughCopy("_includes/assets/scripts");
 
   // sync compiled css
-  eleventyConfig.setBrowserSyncConfig({files: './_site/assets/styles/*.css'});
+  eleventyConfig.setBrowserSyncConfig({ files: "./_site/assets/styles/*.css" });
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
@@ -74,14 +77,15 @@ module.exports = function(eleventyConfig) {
   let options = {
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   };
   let opts = {
-    permalink: false
+    permalink: false,
   };
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt(options).use(markdownItAnchor, opts)
   );
 
   return {
@@ -100,7 +104,7 @@ module.exports = function(eleventyConfig) {
       input: ".",
       includes: "_includes",
       data: "_data",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
